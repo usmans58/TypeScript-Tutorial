26920317124d303bad31c095497c6cc4a1f20860
Usman Siddiqui <usmansiddiqui51@gmail.com>
2024-01-29 18:36:55 +0500

    Arrays

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
+//Basic Types
 let id = 5;
-console.log(id);
+let company = 'Traversy Media';
+let isPublished = true;
+let x = 'Hello';
+x = true;
+let age;
+age = 30;
+//Arrays
+let ids = [1, 2, 3, 4, 5];
+ids.push(6);
+console.log(ids);
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,10 @@ let x: any = 'Hello';
 x=true;
 let age: number;
 age=30;
+
+
+//Arrays
+let ids: number[]= [1,2,3,4,5];
+ids.push(6);
+let arr: any[]= [1,true,'Hello'];
+
